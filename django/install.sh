2594d6fab91cb5c973780028f39efd0696dc2414
JV conseil <contact@jv-conseil.net>
2023-02-20 20:05:08 +0100

    Update install.sh

--- django/install.sh
+++ django/install.sh
@@ -18,23 +18,42 @@
 
 cat "django/README.txt"
 
-if [[ "${DEBUG}" == 1 ]]; then
-  _ucld_::dj_debug
-else
-  python manage.py collectstatic --no-input
-fi
+printf "Please select a valid Django repository:\n"
+select _dj_repo in $(dirname "${PATH_TO_WORK_DIR}"/*/manage.py); do
+  test -n "${_dj_repo}" && break
+  echo ">>> Invalid Selection"
+done
 
-echo
-read -r -N 1 -p "Do you want to install dependencies? [y/N] "
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  _ucld_::dj_install_dependencies
-fi
+if [[ -d "${_dj_repo}" ]]; then
 
-echo
-read -r -N 1 -p "Do you want to run migrations? [y/N] "
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  _ucld_::dj_running_migrations
-  _ucld_::dj_create_superuser
-else
-  python manage.py runserver
+  cd "$_dj_repo" && pwd
+
+  if [[ "${DEBUG}" == 1 ]]; then
+    _ucld_::dj_debug
+  else
+
+    echo
+    read -r -N 1 -p "Do you want to collectstatic? [y/N] "
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      python manage.py collectstatic --no-input
+    fi
+
+  fi
+
+  echo
+  read -r -N 1 -p "Do you want to install dependencies? [y/N] "
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    _ucld_::dj_install_dependencies
+  fi
+
+  echo
+  read -r -N 1 -p "Do you want to run migrations? [y/N] "
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    _ucld_::dj_running_migrations
+    _ucld_::dj_create_superuser
+  else
+    python manage.py runserver
+  fi
+
+  _ucld_::back_to_script_dir_
 fi
