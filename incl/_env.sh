e9e6ef8729c45864aae13c693e8da2583b2c6e64
JV conseil <contact@jv-conseil.net>
2023-02-21 15:52:52 +0100

    Update _env.sh

--- incl/_env.sh
+++ incl/_env.sh
@@ -6,6 +6,9 @@
 # licence       : BSD 3-Clause License
 # copyright     : Copyright (c) 2019-2023 JV-conseil
 #                 All rights reserved
+#
+# shellcheck disable=SC2034
+#
 #====================================================
 
 # shellcheck disable=SC1091
@@ -22,9 +25,9 @@ if [[ -z ${PATH_TO_SCRIPT_DIR} ]]; then
   readonly PATH_TO_SCRIPT_DIR="${PWD}"
   readonly PATH_TO_WORK_DIR="${PATH_TO_SCRIPT_DIR%/*}"
 
-fi
+  readonly PATH_TO_DATA_DIR="${PATH_TO_WORK_DIR}/${UCLD_DATA_DIR}"
+  readonly PATH_TO_INSTALL_DIR="${PATH_TO_WORK_DIR}/${UCLD_INSTALL_DIR}"
+  readonly PATH_TO_ENV="${PATH_TO_WORK_DIR}/${UCLD_ENV_DIR}/.env"
+  readonly PATH_TO_PGPASS="${PATH_TO_WORK_DIR}/${UCLD_ENV_DIR}/.pgpass"
 
-export PATH_TO_DATA_DIR="${PATH_TO_WORK_DIR}/${UCLD_DATA_DIR}"
-export PATH_TO_INSTALL_DIR="${PATH_TO_WORK_DIR}/${UCLD_INSTALL_DIR}"
-export PATH_TO_ENV="${PATH_TO_WORK_DIR}/${UCLD_ENV_DIR}/.env"
-export PATH_TO_PGPASS="${PATH_TO_WORK_DIR}/${UCLD_ENV_DIR}/.pgpass"
+fi
