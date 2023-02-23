093fa6f750bde7de27b3df7809b110b72985cdb9
JV conseil <contact@jv-conseil.net>
2023-02-23 19:05:16 +0100

    remove declare -r 🙅‍♂️

--- incl/_env.sh
+++ incl/_env.sh
@@ -11,37 +11,33 @@
 #
 #====================================================
 
-if ! [ -v "${UCLD_PATH}" ]; then
+declare -a UCLD_INSTALL_PACKAGES
+declare -A UCLD_DB_PARAM UCLD_DIR UCLD_PATH UCLD_PG_PATH
+declare -xi DEBUG
 
-  declare -a UCLD_INSTALL_PACKAGES
-  declare -A UCLD_DB_PARAM UCLD_DIR UCLD_PATH UCLD_PG_PATH
-  declare -xi DEBUG
+# shellcheck disable=SC1091
+{
+  . "./settings.conf"
+  . "env/settings.conf" &>/dev/null
+  # more files
+}
 
-  # shellcheck disable=SC1091
-  {
-    . "./settings.conf"
-    . "env/settings.conf" &>/dev/null
-    # more files
-  }
+UCLD_PATH["main"]="${PWD}"
+UCLD_PATH["work"]="${UCLD_PATH[main]%/*}"
 
-  UCLD_PATH["main"]="${PWD}"
-  UCLD_PATH["work"]="${UCLD_PATH[main]%/*}"
+# export UCLD_PATH[main]="${PWD}"
+# export UCLD_PATH[work]="${UCLD_PATH[main]%/*}"
 
-  # export UCLD_PATH[main]="${PWD}"
-  # export UCLD_PATH[work]="${UCLD_PATH[main]%/*}"
+for key in "${!UCLD_DIR[@]}"; do
 
-  for key in "${!UCLD_DIR[@]}"; do
+  _value="${UCLD_PATH[work]}/${UCLD_DIR[${key}]}"
+  UCLD_PATH["${key}"]="${_value}"
 
-    _value="${UCLD_PATH[work]}/${UCLD_DIR[${key}]}"
-    UCLD_PATH["${key}"]="${_value}"
+  # globals
+  if [[ "${key}" == "data" ]]; then
+    eval "export UCLD_PATH_TO_${key^^}=\"${_value}\""
+  fi
 
-    # globals
-    if [[ "${key}" == "data" ]]; then
-      eval "export UCLD_PATH_TO_${key^^}=\"${_value}\""
-    fi
+done
 
-  done
-
-  declare -r DEBUG UCLD_DIR UCLD_PATH
-
-fi
+DEBUG UCLD_DIR UCLD_PATH
