a3a3ba6e3a9f5d0d6eb513375a066317d6f62116
JV conseil <contact@jv-conseil.net>
2023-02-25 16:59:29 +0100

    backup
    Signed-off-by: JV conseil <contact@jv-conseil.net>

--- postgresql/_env.sh
+++ postgresql/_env.sh
@@ -11,13 +11,22 @@
 #
 #====================================================
 
+# for key in ".pgpass" ".env"; do
+#   UCLD_PG_PATH["${key}"]="${UCLD_PG_PATH[env]}/${key}"
+# done
+
+_ucld_::assign_path
+
 UCLD_DB_PARAM["password"]="$(_ucld_::key_gen 32)"
 
-for key in ".pgpass" ".env"; do
-  UCLD_PG_PATH["${key}"]="${UCLD_PG_PATH[env]}/${key}"
-done
-
-export PGPASSFILE="${UCLD_PG_PATH[".pgpass"]}"
-export PGSSLROOTCERT="${UCLD_PG_PATH[env]}/server.cert.pem"
-export PGSSLMODE="${UCLD_DB_PARAM[sslmode]}"
-export PGUSER="ucloud"
+{
+  export PGPASSFILE="${UCLD_PG_PATH[".pgpass"]}"
+  export PGSSLROOTCERT="${UCLD_PG_PATH[env]}/server.cert.pem"
+  export PGSSLMODE="${UCLD_DB_PARAM[sslmode]}"
+  export PGUSER="ucloud"
+  # } 2>>logfile.log &
+} 2> >(
+  while read -r _line; do
+    printf "%s\t%s\n" "$(date "+%Y-%m-%d %H:%M:%S")" "${_line}" >>logfile.log
+  done >&2
+) &
