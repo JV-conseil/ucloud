f241e7c0b94057aa69d737db4c3ae173b7f8b92f
JV conseil <contact@jv-conseil.net>
2023-02-27 20:15:27 +0100

    UCLD_SETTINGS_PATH

--- incl/_settings.sh
+++ incl/_settings.sh
@@ -8,16 +8,16 @@
 #                 All rights reserved
 #====================================================
 
+declare -a UCLD_SETTINGS_PATH
+
+UCLD_SETTINGS_PATH=("app" "django")
+
 _ucld_::edit_settings() {
   nano "${UCLD_PATH[env]}/settings.conf"
   _ucld_::source_settings
 }
 
 _ucld_::show_settings() {
-  if [[ "${DEBUG}" -eq 0 ]]; then
-    return
-  fi
-  _ucld_::h3 "Your settings can be modified with\nnano ${UCLD_PATH[env]}/settings.conf"
   cat "${UCLD_PATH[env]}/settings.conf"
 }
 
@@ -28,9 +28,13 @@ _ucld_::source_settings() {
 }
 
 _ucld_::reset_settings() {
+  local _path
   rm -v "${UCLD_PATH[env]}/settings.conf"
   cp -v "./settings.conf" "${UCLD_PATH[env]}/settings.conf"
   _ucld_::source_settings
+  for _key in "${UCLD_SETTINGS_PATH[@]}"; do
+    unset "${UCLD_SETTINGS_PATH["${_key}"]}" 2>logfile
+  done
 }
 
 _ucld_::update_settings() {
