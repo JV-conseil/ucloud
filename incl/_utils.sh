63c85076182fb58211d8c6aea5d8c1f85178c6b6
JV conseil <contact@jv-conseil.net>
2023-03-04 17:26:53 +0100

    _ucld_::sanitize_input

--- incl/_utils.sh
+++ incl/_utils.sh
@@ -12,14 +12,6 @@ _ucld_::back_to_script_dir_() {
   cd_ "${UCLD_PATH[main]}"
 }
 
-_ucld_::parent_directory() {
-  # <https://stackoverflow.com/a/24112741/2477854>
-  echo "$(
-    cd_ "$(dirname "${BASH_SOURCE[0]}")"
-    pwd -P
-  )"
-}
-
 _ucld_::is_python_installed() {
   local _bool=false
   if [ -x "$(command -v python)" ]; then _bool=true; fi
@@ -30,6 +22,32 @@ _ucld_::is_ucloud_execution() {
   local _bool=false
   if [[ "${PWD}" == "/work/"* ]]; then _bool=true; fi
   echo ${_bool}
+}
+
+# e.g.: $(_ucld_::key_gen 128)
+_ucld_::key_gen() {
+  local _size=${1:-15}
+  # if [ -x "$(command -v python)" ]; then
+  if "$(_ucld_::is_python_installed)"; then
+    python -c "import secrets; result = ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-+') for i in range($_size)); print(result)"
+  else
+    openssl rand -base64 "${_size}"
+  fi
+}
+
+_ucld_::parent_directory() {
+  # <https://stackoverflow.com/a/24112741/2477854>
+  echo "$(
+    cd_ "$(dirname "${BASH_SOURCE[0]}")"
+    pwd -P
+  )"
+}
+
+# sanitize input using a regular expression
+_ucld_::sanitize_input() {
+  local _bool=false
+  if [[ "${1}" =~ ^[a-zA-Z0-9_./-]+$ ]]; then _bool=true; fi
+  echo "${_bool}"
 }
 
 _ucld_::update_bashrc() {
@@ -45,15 +63,4 @@ _ucld_::update_bashrc() {
 #====================================================" >>"${HOME}/${_file}"
     cat incl/_aliases.sh >>"${HOME}/${_file}"
   done
-}
-
-_ucld_::key_gen() {
-  # e.g.: $(_ucld_::key_gen 128)
-  local _size=${1:-15}
-  # if [ -x "$(command -v python)" ]; then
-  if "$(_ucld_::is_python_installed)"; then
-    python -c "import secrets; result = ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-+') for i in range($_size)); print(result)"
-  else
-    openssl rand -base64 "${_size}"
-  fi
 }
