8e361d9eb327c6cab802643f731d4a11b04d0dee
JV conseil <contact@jv-conseil.net>
2023-03-07 16:01:20 +0100

    Update all.sh

--- incl/all.sh
+++ incl/all.sh
@@ -10,17 +10,17 @@
 
 # shellcheck source=/dev/null
 {
-  . "incl/__set.sh"
-  . "incl/__aliases.sh"
-  . "incl/__debug.sh"
-  . "incl/__utils.sh"
-  . "incl/__colors.sh"
+  . "incl/__set.sh" || "/work/ucloud/incl/__set.sh"
+  . "incl/__aliases.sh" || "/work/ucloud/incl/__aliases.sh"
+  . "incl/__debug.sh" || "/work/ucloud/incl/__debug.sh"
+  . "incl/__utils.sh" || "/work/ucloud/incl/__utils.sh"
+  . "incl/__colors.sh" || "/work/ucloud/incl/__colors.sh"
   # below this line depends on ./settings.conf
-  . "incl/_path.sh"
-  . "incl/_exceptions.sh"
-  . "incl/_skeleton.sh"
-  . "incl/_settings.sh"
-  . "incl/_install.sh"
-  . "incl/_ssh.sh"
+  . "incl/_path.sh" || "/work/ucloud/incl/_path.sh"
+  . "incl/_exceptions.sh" || "/work/ucloud/incl/_exceptions.sh"
+  . "incl/_skeleton.sh" || "/work/ucloud/incl/_skeleton.sh"
+  . "incl/_settings.sh" || "/work/ucloud/incl/_settings.sh"
+  . "incl/_install.sh" || "/work/ucloud/incl/_install.sh"
+  . "incl/_ssh.sh" || "/work/ucloud/incl/_ssh.sh"
   # more files
 }
